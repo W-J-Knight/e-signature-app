b738799bd82892b16556fc04b547ae345fd22132
William Knight <William.Jeffery.Knight@protonmail.com>
2023-05-03 08:58:42 -0400

    Use useState to make the website interact

--- src/Page/Home.jsx
+++ src/Page/Home.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import Title from "../components/Title";
 
 export default function Home() {
+
+  const [name, setName] = useState("Your Signature")
+  const [date, setDate] = useState("Select DATE")
+  const handleNameChange = (e) => {
+    // console.log(e.target.value)
+    setName(previousName => previousName = e.target.value);
+  }
+  const handleDateChange = (e) => {
+    setDate(previousDate => previousDate = e.target.value);
+  };
   const inputStyle = {
     border: "none",
     borderBottom: " 2px dotted",
@@ -12,23 +22,36 @@ export default function Home() {
   document.body.style.background = "#eee";
 
   return (
-    <div className="container text-center">
-      <Title classes={"title"} text={"Name"} />
-      <Title classes={"main-title mb-4"} text={"Date"} />
+    <div className="container text-center text-uppercase">
+      {/* <Title classes={"title"} text={"Name"} /> */}
+      <Title classes={"title"} text={name} />
+      <Title classes={"main-title mb-4"} text={date} />
       <p>
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum
         voluptatum voluptatem laudantium nobis illo obcaecati, minima incidunt
         praesentium, aut vel inventore necessitatibus neque sequi similique ut
         nisi libero? Esse, eum!
       </p>
-      <footer className="d-flex" 
-      style={{
-        justifyContent: "space-around",
-        position: "relative",
-        top: "40vh"
-        }}>
-      <input type="date" value={""} style={inputStyle} />
-      <input type="text" value={""} style={inputStyle} />
+      <footer
+        className="d-flex"
+        style={{
+          justifyContent: "space-around",
+          position: "relative",
+          top: "40vh",
+        }}
+      >
+        <input
+          type="date"
+          value={date}
+          style={inputStyle}
+          onChange={handleDateChange}
+        />
+        <input
+          type="text"
+          value={name}
+          style={inputStyle}
+          onChange={handleNameChange}
+        />
       </footer>
     </div>
   );
